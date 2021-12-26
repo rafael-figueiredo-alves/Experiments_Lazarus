916f1f18b878fb465190d6ef1e0d9c531b30efbe
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2021-12-26 19:44:37 -0300

    Add HelloWorld
    Added example of HelloWorld

---
+++ HelloWorld/View/helloworld.view.main.pas
@@ -0,0 +1,38 @@
+unit HelloWorld.View.Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  showmessage('Hello World from Lazarus!!!!');
+end;
+
+end.
+
